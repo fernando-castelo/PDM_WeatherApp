b0c911169dfc6f3611545f878c0e3e7ab4f57923
fernando-castelo <nando.castelobranco24@hotmail.com>
2025-06-04 22:31:33 -0300

    feat(): Primeira Pratica

--- app/src/main/java/com/example/pdm_weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/pdm_weatherapp/MainActivity.kt
@@ -1,29 +1,88 @@
 package com.example.pdm_weatherapp
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.pdm_weatherapp.ui.theme.PDM_WeatherAPPTheme
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import com.example.weatherapp.ui.nav.BottomNavBar
+import com.example.weatherapp.ui.nav.BottomNavItem
+import com.example.weatherapp.ui.nav.MainNavHost
+import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PDM_WeatherAPPTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val navController = rememberNavController()
+            WeatherAppTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("Bem-vindo/a!") },
+
+                            actions = {
+
+                                IconButton( onClick = { finish() } ) {
+                                    Icon(
+                                        imageVector =
+                                            Icons.AutoMirrored.Filled.ExitToApp,
+                                        contentDescription = "Localized description"
+                                    )
+                                }
+                            }
+                        )
+                    },
+
+                    bottomBar = {
+                        val items = listOf(
+                            BottomNavItem.HomeButton,
+                            BottomNavItem.ListButton,
+                            BottomNavItem.MapButton,
+
+                            )
+
+                        BottomNavBar(navController = navController, items)
+
+                    },
+
+                    floatingActionButton = {
+
+                        FloatingActionButton(onClick = { }) {
+                            Icon(Icons.Default.Add, contentDescription = "Adicionar")
+                        }
+                    }
+                ) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        MainNavHost(navController = navController)
+                    }
                 }
             }
         }
@@ -31,17 +90,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun HomePage(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val activity = context as? Activity
+    Column(
         modifier = modifier
-    )
-}
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Bem-vindo à HomePage!")
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PDM_WeatherAPPTheme {
-        Greeting("Android")
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = {
+            activity?.finish()
+        }) {
+            Text("Sair")
+        }
     }
 }
