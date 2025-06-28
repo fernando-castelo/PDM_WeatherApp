5dc0cc1734dee62df652c357b7164b41e5d07f6f
fernando-castelo <nando.castelobranco24@hotmail.com>
2025-06-27 23:52:24 -0300

    feat: Pratica 4 - Parte 4

--- app/src/main/java/com/example/pdm_weatherapp/MapPage.kt
+++ app/src/main/java/com/example/pdm_weatherapp/MapPage.kt
@@ -1,22 +1,31 @@
 package com.example.pdm_weatherapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -29,9 +38,23 @@ fun MapPage(viewModel: MainViewModel) {
     val joaopessoa = LatLng(-7.12, -34.84)
     val camPosState = rememberCameraPositionState ()
 
-    GoogleMap (modifier = Modifier.fillMaxSize(), cameraPositionState = camPosState, onMapClick = {
-        viewModel.add("Cidade@${it.latitude}:${it.longitude}", location = it)
-    }) {
+    val context = LocalContext.current
+    val hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED
+        )
+    }
+
+
+    GoogleMap (
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = camPosState,
+        onMapClick = { viewModel.add("Cidade@${it.latitude}:${it.longitude}", location = it) },
+        properties = MapProperties(isMyLocationEnabled = hasLocationPermission),
+        uiSettings = MapUiSettings(myLocationButtonEnabled = true)
+    ) {
         viewModel.cities.forEach {
             if (it.location != null) {
                 Marker( state = MarkerState(position = it.location),
