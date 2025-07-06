5ffee27f1d01655fe524559ed38f5e9ec699b5ee
Silva <f.castelo.branco@accenture.com>
2025-07-06 13:16:49 -0300

    feat: Pratica 6 | Parte 2

--- app/src/main/java/com/example/pdm_weatherapp/MainViewModel.kt
+++ app/src/main/java/com/example/pdm_weatherapp/MainViewModel.kt
@@ -1,11 +1,18 @@
 package com.example.pdm_weatherapp
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 import com.example.pdm_weatherapp.model.City
+import com.example.pdm_weatherapp.model.User
 import com.google.android.gms.maps.model.LatLng
 
 class  MainViewModel : ViewModel() {
+
+    private val _user = mutableStateOf<User?> (null)
+    val user : User?
+        get() = _user.value
+
     private val _cities = getCities().toMutableStateList()
     val cities
         get() = _cities.toList()
@@ -20,3 +27,5 @@ class  MainViewModel : ViewModel() {
 private fun getCities() = List(20) { i ->
     City(name = "Cidade $i", weather = "Carregando clima...")
 }
+
+
---
+++ app/src/main/java/com/example/pdm_weatherapp/model/User.kt
@@ -0,0 +1,4 @@
+package com.example.pdm_weatherapp.model
+
+data class User(val name: String, val email: String)
+
