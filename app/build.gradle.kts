ec90fea9de3e3cda77b5e71b17baf0a8ac6a222b
Silva <f.castelo.branco@accenture.com>
2025-07-06 13:03:04 -0300

    chore: Pratica 6 | Parte 1 - Dependencias Cloud Firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.firebase.common.ktx)
     implementation(libs.firebase.auth.ktx)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
